b44aff2f85a37f3a6e09e1d0b620b55ebc386103
webxukai <790134972@qq.com>
2019-12-30 13:56:11 +0800

    .

--- kankan-ser/config/sql.js
+++ kankan-ser/config/sql.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2019-12-26 17:54:49
- * @LastEditTime : 2019-12-27 15:32:35
+ * @LastEditTime : 2019-12-30 13:44:44
  * @LastEditors  : Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \kankan\kankan-ser\config\sql.js
@@ -10,6 +10,8 @@ module.exports = {
     port: 3306,
     password: 'QWasz12.-',
     database: 'kkuser',
-    host: 'localhost',
-    user: 'root'
+    port: '3306', //端口
+    host: '129.211.47.103',
+    user: 'root',
+    multipleStatements: true //允许执行多条语句
   }
